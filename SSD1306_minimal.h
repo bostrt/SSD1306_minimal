c4254e8b62747d877d5072384e6076b3d113168e
Kirk Northrop <kirk@krn.me.uk>
2016-03-12 19:56:04 +0000

    Update complicated licence agreements

--- SSD1306_minimal.h
+++ SSD1306_minimal.h
@@ -3,28 +3,33 @@
 
 
 /*
-
   SSD1306_minimal.h - SSD1306 OLED Driver Library
-  2015 Copyright (c) CoPiino Electronics All right reserved.
   
-  Original Author: GOF Electronics Co. Ltd.
-  Modified by: CoPiino Electronics ( http://copiino.cc ) 
+  Copyright (c) 2012, Adafruit Industries. All rights reserved.
+  Copyright (c) 2012 GOF Electronics Co. Ltd ( http://www.geekonfire.com )
+  Copyright (c) 2015 CoPiino Electronics All right reserved.
+  Copyright (c) 2016 Kirk Northrop (github.com/kirknorthrop)
   
-  CoPiino Electronics invests time and resources providing this open source code, 
-	please support CoPiino Electronics and open-source hardware by purchasing 
-	products from CoPiino Electronics!
-  
-  What is it?	
-    This library is derived from GOFi2cOLED library, only for SSD1306 in I2C Mode.
-    As the original library only supports Frame Buffered mode which requires to have
-    at least 1024bytes of free RAM for a 128x64px display it is too big for smaller devices.
-    
-    So this a SSD1306 library that works great with ATTiny85 devices :)
-    
-  
+  Original Author: Limor Fried/Ladyada Adafruit Industries
+  Modified by: Jimbo.we (www.geekonfire.com)
+  Modified by: CoPiino Electronics (http://copiino.cc)
+
+      CoPiino Electronics invests time and resources providing this open source code,
+      please support CoPiino Electronics and open-source hardware by purchasing
+      products from CoPiino Electronics!
+
+      What is it?
+        This library is derived from GOFi2cOLED library, only for SSD1306 in I2C Mode.
+        As the original library only supports Frame Buffered mode which requires to have
+        at least 1024bytes of free RAM for a 128x64px display it is too big for smaller devices.
+
+        So this a SSD1306 library that works great with ATTiny85 devices :)
+
+  Modified by: Kirk Northrop (github.com/kirknorthrop)
+
   It is a free software; you can redistribute it and/or modify it 
-  under the terms of BSD license, check license.txt for more information.
-	All text above must be included in any redistribution.
+  under the terms of BSD license, check LICENSE for more information.
+  All text above must be included in any redistribution.
 */
 
 
